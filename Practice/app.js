871277b4465ac0946650745d90d31a4b90bb2423
Rohan Baranwal <baranwal.rohan0@gmail.com>
2022-09-12 09:02:01 +0530

    created static index.html

--- Practice/app.js
+++ Practice/app.js
@@ -1,13 +1,23 @@
 import express from "express";
 import chalk from "chalk";
-import debug from 'debug';
+import debug from "debug";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const debugApp = debug("app");
 const app = express();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
+
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
   res.send("Hello from my  app");
 });
 
 app.listen(3000, () => {
-  console.log(`Listening on port ${chalk.green("3000")}`);
+  debugApp(`Listening on port ${chalk.green("3000")}`);
 });
